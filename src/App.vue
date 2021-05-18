84eda408cdebd8c0373bb8a441ffd7c7d96596ce
Guilherme Araujo <gui--08@hotmail.com>
2021-05-18 15:40:32 -0300

    virgulas corrigidas em export defalt script

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,24 @@
 <template>
-
   <img src="./assets/capivara.png" alt="" srcset="">
-  
   <Chaybs msg="tem que retornar string aq porra" numero="25" />
-
   <img  alt="Pegasus assustado" src="./assets/proterzeu.png">
   <Gaba msg="É HORA DO DUELO" :numero="24"/>
-
   <img alt="Vue logo" src="./assets/logo.png">
-
   <HelloWorld msg="Welcome to Your Vue.js App"/>
-
 </template>
-
-
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Gaba from './components/Gaba.vue'
-
 import Chaybs from './components/Chaybs'
-
-
 export default {
   name: 'App',
   components: {
-    Gaba
-    Chaybs
+    Gaba,
+    Chaybs,
     HelloWorld,
   }
 }
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
