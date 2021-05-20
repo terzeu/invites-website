42b994e81022995781f41009e92524161e41c381
Guilherme Araujo <gui--08@hotmail.com>
2021-05-20 18:03:12 -0300

    segunda atividade

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <img src="./assets/capivara.png" alt="" srcset="">
-  <Chaybs msg="tem que retornar string aq porra" numero="25" />
-  <img  alt="Pegasus assustado" src="./assets/proterzeu.png">
-  <Gaba msg="É HORA DO DUELO" :numero="24"/>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Chaybs nomeDasCapiva="capivilda" descricao="BRABONAAAA" idade="10" :tipoanu="true"/>
+  <img  alt="capivaras" src="./assets/capivara.png">
+  <Chaybs nomeDasCapiva="juranilda" descricao="DOCIL E CALMA" idade="15" :tipoanu="false"/>
+  <img  alt="capivaras" src="./assets/capivara.png">
+  <!--<Gaba msg="É HORA DO DUELO" :numero="24"/>!-->
+  //<HelloWorld msg="Welcome to Your Vue.js App"/> 
 </template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Gaba from './components/Gaba.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+//import Gaba from './components/Gaba.vue'
 import Chaybs from './components/Chaybs'
 export default {
   name: 'App',
   components: {
-    Gaba,
+   //Gaba,
     Chaybs,
-    HelloWorld
+    //HelloWorld
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 createApp(App).mount('#app')
+window.alert('me desculpe pela merda lineu :(.')
+
+
