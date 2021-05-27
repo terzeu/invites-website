1ae070cc1a737814c6232bcabf2f36b22d092588
gaba.araujo <gabriel-ara-sou@hotmail.com>
2021-05-27 16:05:08 -0300

    excluindo arquivos que nao iremos usar

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
 <template>
-  <Chaybs nomeDasCapiva="capivilda" descricao="BRABONAAAA" idade="10" :tipoanu="true"/>
-  <img class="teste" src="./assets/capivara.png" alt="" srcset="">
-  <Chaybs nomeDasCapiva="juranilda" descricao="DOCIL E CALMA" idade="15" :tipoanu="false"/>
-  <Gaba msg="É HORA DO DUELO?" :numero="27"/>
 </template>
 <script>
-import Gaba from './components/Gaba.vue'
-import Chaybs from './components/Chaybs'
 export default {
   name: 'App',
   components: {
-    Gaba,
-    Chaybs
+
   }
 }
 </script>
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
