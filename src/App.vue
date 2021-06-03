1d5e91fd9724b4a5f180c52ee8946eba5ec8837f
Guilherme Araujo <gui--08@hotmail.com>
2021-06-02 22:16:48 -0300

    flexbox adicionados e espacos arrumados

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
-
   <cabeca/>
 </template>
 <script>
 import Cabeca from './components/Cabeca.vue'
 export default {
   name: 'App',
-
   components: {
     Cabeca
-  
   }
 }
 </script>
 <style lang="scss"> 
 @import "./assets/app.scss";
+body {
+  height: calc(100vh - 150px);
+  margin: 0;
+  background: no-repeat center/cover url('./assets/images/madeira.jpg');
+}
 #app {
   font-family: 'Sofia';
   -webkit-font-smoothing: antialiased;
--- src/components/Cabeca.vue
+++ src/components/Cabeca.vue
@@ -1,47 +1,58 @@
 <template>
 <header class="cabess">
-    <nav>
+    <nav class="nav-header">
       <p>Formatura</p>
-      <ul>
-        <a href="localhost:8080"><li>inicio</li></a>
-        <li>local</li>
-         <li>confirmar presença</li>
+      <ul class="tittle-header">
+        <a href="#"><li class="ini">inicio</li></a>
+        <a href="#"><li class="loc">local</li></a>
+        <a href="#"><li class="conf">confirmar presença</li></a>
       </ul>
     </nav>
 </header>
 </template>
 <script>
 export default{
-
 }
 </script>
-
 <style>
-
-body{
-  height: calc(100vh - 150px);
-  margin: 0;
-  background: no-repeat center/cover url('../assets/images/madeira.jpg');
-  
+a {
+  text-decoration: none;
 }
-
-.cabess{
+.cabess {
+  position: fixed;
+  width: 100%;
   background: rgba(255, 192, 203, 0.514);  
   font-size: 25px;
   margin-top: -69px;
-  cursor: pointer;
 }
-ul{
-  list-style: none;
+.nav-header {
   display: flex;
-  
-}
-li{
-  margin: 0 30px;
-}
-nav{
-  display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   margin: auto 80px;
+}
+.tittle-header {
+  display: flex;
+  list-style: none;
+}
+.ini {
+  margin: 0 30px;
+  color: #2c3e50;
+}
+.loc {
+  margin: 10 30px;
+  color: #2c3e50;  
+}
+.conf {
+  margin: 0 30px;
+  color: #2c3e50;
+}
+.ini:active {
+  color: red;
+}
+.loc:active {
+  color: red;
+}
+.conf:active {
+  color: red;
 }
 </style>
