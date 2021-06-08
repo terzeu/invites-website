421a89fa25c62730a3312fa7d3638d4736c18295
gaba.araujo <gabriel-ara-sou@hotmail.com>
2021-06-08 17:07:47 -0300

    Pagina dois em progresso

--- src/views/Location.vue
+++ src/views/Location.vue
@@ -1,28 +1,52 @@
 <template>
-  <div class="location-page">
-    <div class="location-title">
-      <p>ONDE VAI SER?</p>
-      <font-awesome-icon :icon="['fas', 'users']" />
-      <p> QUANDO?</p>
-      <p> MAPA DO LOCAL?</p>
+  <div class="location-start">
+    <h2>ONDE VAI SER?</h2>
+    <div class="location-page">
+      <div class="location-data">
+        <p><font-awesome-icon icon="clock" />QUANDO?<br>
+        Quinta-feira, 08 de Setembro de 2022 a partir das 20:00</p>
+      </div>
+      <div class="location-description">
+        <p><font-awesome-icon icon="clock" />AONDE?<br>
+        Hotel Fazenda Pirapitinga, Luziânia GO 061</p>
+      </div>
+      <div class="location-map">
+        <p><font-awesome-icon icon="clock" />MAPA<br>
+        MAPA AQUI</p>
+      </div>
     </div>
   </div>
 </template>
 <script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faClock)
 export default {
   name: 'Location'
 }
 </script>
 <style lang="scss" scoped>
-.location-page {
+.location-start {
   color: rgb(223, 223, 223);
   font-size: 33px;
   text-shadow: 2px 3px 5px black;
   height: 100vh;
   background: no-repeat center/cover url('../assets/images/madeira2.jpg');
   display: flex;
-  flex-direction: column; 
-  align-items: center;
-  justify-content: center;
+  .location-page{
+    justify-content:flex-end; 
+    text-align: left;
+    .location-data{
+    
+    }
+    .location-description{
+      font-size: 30px;
+    }
+    .location-map{
+      
+    }
+  }
+  
 }   
 </style>
