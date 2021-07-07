b146f8a82568f11060196df99a653662a96c50c4
gaba.araujo <gabriel-ara-sou@hotmail.com>
2021-07-07 15:18:52 -0300

    atualizando lista de convidados e preparando o database do firebase

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,33 @@
 <script>
 import Header from './components/Header.vue'
 import Router from './components/Router.vue'
+import pessoa from './peoples'
+import Firebase from 'firebase'
+var firebaseConfig = {
+    apiKey: "AIzaSyAbDDYhpS3DOteVl7sYiPnRSR3dpqcPCB4",
+    authDomain: "conviteformatura-2baf2.firebaseapp.com",
+    databaseURL: "https://conviteformatura-2baf2-default-rtdb.firebaseio.com",
+    projectId: "conviteformatura-2baf2",
+    storageBucket: "conviteformatura-2baf2.appspot.com",
+    messagingSenderId: "826582283350",
+    appId: "1:826582283350:web:22c47603c551d6590e77f7"
+}
+Firebase.initializeApp(firebaseConfig)
+const every = Firebase.database().ref().child('Convidados')
+
 export default {
   name: 'App',
   components: {
     Header,
     Router
+  },
+  data() {
+   return { pessoa } 
+  },
+  method: {
+    add() {
+      every.push(pessoa)
+    }
   }
 }
 </script>
